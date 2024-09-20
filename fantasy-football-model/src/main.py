773095fb993f5d990f4c9964d7ad033150fe8459
Kellen Bavis <kellenbavis10@gmail.com>
2024-09-20 18:52:31 -0400

    feat: re-organized project structure

--- fantasy-football-model/src/main.py
+++
@@ -1,4 +0,0 @@
-'''
-   Main entry point of our application that will initate web scraping, cleaning of data, 
-   persisting of data, generation of model, generation of predictions, and generating output
-'''
