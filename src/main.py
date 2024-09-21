47c1d0adc226387195b79df18da3a734fd3c4658
Kellen Bavis <kellenbavis10@gmail.com>
2024-09-21 00:24:11 -0400

    feat: utilizing new log & yaml config in main

--- src/main.py
+++ src/main.py
@@ -1,19 +1,25 @@
+import logging
+from config.yaml_config import YamlConfig
+from scraping.scraping_utility import ScrapingUtility
+from config.logger_config import LoggerConfig
+
 '''
    Main entry point of our application that will initate web scraping, cleaning of data, 
    persisting of data, generation of model, generation of predictions, and generating output
 '''
-
-import logging 
-from scraping.scraping_utility import ScrapingUtility
-
 def main():
-   #TODO: Create Logging Config File 
-   logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s') 
-   logging.info("Beginning scraping process") 
+   #Load Configurations
+   config = YamlConfig("./config/application.yaml")
+    
+   #Create Logger 
+   logConfig = LoggerConfig(logging.INFO, "%(asctime)s - %(levelname)s - %(message)s")
+   log = logConfig.get_logger()
    
    #Scrape for each URL 
    scrapingUtility = ScrapingUtility()
-   htmlText = scrapingUtility.fetchPage("https://www.pro-football-reference.com/") #TODO: Add URLs to Application YAML File 
+   for url in config.get_websites(): 
+      log.info(f"Fetching HTML content for the following URL: {url}")
+      htmlText = scrapingUtility.fetchPage(url);
    
 
 
