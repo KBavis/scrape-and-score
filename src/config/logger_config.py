3c0a4a47bd17eb09664d22a619ecc9b14dcc3e7f
Kellen Bavis <kellenbavis10@gmail.com>
2024-09-21 00:23:52 -0400

    feat: added logging config

---
+++ src/config/logger_config.py
@@ -0,0 +1,13 @@
+import logging;
+
+class LoggerConfig:
+   def __init__(self, level, format): 
+      self._level = level
+      self._format = format 
+      self.configure_logger() 
+   
+   def configure_logger(self):
+      logging.basicConfig(level=self._level, format=self._format)
+      
+   def get_logger(self):
+      return logging.getLogger(__name__)
