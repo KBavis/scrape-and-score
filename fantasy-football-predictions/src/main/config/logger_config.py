db7ebaceb26e33097c6ac225a23ab46206d0ca63
Kellen Bavis <kellenbavis10@gmail.com>
2024-09-25 18:30:31 -0400

    refactor: switching from OO to functional approach

--- fantasy-football-predictions/src/main/config/logger_config.py
+++ fantasy-football-predictions/src/main/config/logger_config.py
@@ -1,10 +1,6 @@
 import logging;
 
-class LoggerConfig:
-   def __init__(self, level, format): 
-      self._level = level
-      self._format = format 
-      self.configure_logger() 
-   
-   def configure_logger(self):
-      logging.basicConfig(level=self._level, format=self._format)
+def configure_logging():
+    logging.basicConfig(level=logging.INFO,  
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S') 
