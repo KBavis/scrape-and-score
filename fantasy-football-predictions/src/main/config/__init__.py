ba473dcf35884ce00a6c8e3c767c431b22f43bfd
Kellen Bavis <kellenbavis10@gmail.com>
2024-10-01 19:06:00 -0400

    feat: adding package declaration file updates to give other packages access to variables

--- fantasy-football-predictions/src/main/config/__init__.py
+++ fantasy-football-predictions/src/main/config/__init__.py
@@ -1,1 +1,2 @@
 from .logger_config import configure_logging
+from .props import load_configs
---
+++ fantasy-football-predictions/src/main/data/__init__.py
@@ -0,0 +1,3 @@
+from .pro_football_reference.pro_football_reference_data import team_hrefs, months, locations, cities
+
+__all__ = ['team_hrefs', 'months', 'locations', 'cities']
