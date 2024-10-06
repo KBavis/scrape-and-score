cac7004525024ea55b4e4b6705a104bfcb1bbf5c
Kellen Bavis <kellenbavis10@gmail.com>
2024-10-06 02:35:09 -0400

    feat: added logging within main

--- fantasy-football-predictions/src/main/main.py
+++ fantasy-football-predictions/src/main/main.py
@@ -2,6 +2,7 @@ from config import configure_logging
 from scraping import scrape_fantasy_pros
 from scraping import scrape_pro_football_reference
 from config import load_configs
+import logging
 
 
 '''
@@ -17,10 +18,12 @@ def main():
    
    #Fetch relevant NFL teams & players
    teams_and_players = scrape_fantasy_pros(config['website']['fantasy-pros']['urls']['depth-chart'])
+   logging.info("Successfully fetched fantasy relevant NFL teams/players")
    
    #Fetch relevant team and player metrics 
    team_metrics, player_metrics = scrape_pro_football_reference(teams_and_players, config['nfl']['current-year'], config)
-
+   logging.info(f"Successfully retrieved metrics for {len(team_metrics)} teams and {len(player_metrics)} players")
+   
 # Entry point for the script
 if __name__ == "__main__":
     main()
