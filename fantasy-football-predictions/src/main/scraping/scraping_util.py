0cb7b09c8ebffb435a7bbe4254271d081c9566c6
Kellen Bavis <kellenbavis10@gmail.com>
2024-10-06 02:32:55 -0400

    feat: added session based requests and proxies

--- fantasy-football-predictions/src/main/scraping/scraping_util.py
+++ fantasy-football-predictions/src/main/scraping/scraping_util.py
@@ -1,6 +1,8 @@
 import requests
 import logging 
-from swiftshadow import QuickProxy
+from swiftshadow.classes import Proxy
+import time 
+session = requests.Session() 
 
 '''
 Functionality to fetch the raw HTML corresponding to a particular URL 
@@ -11,17 +13,36 @@ Args:
 Returns:
    raw HTML corresponding to specified URL   
 '''
+
 def fetch_page(url: str):
    try:
       #TODO: Implement Rotating Proxy Logic (FFM-12)
       logging.info(f"Fetching raw HTML from the following URL: {url}")
-      proxy = QuickProxy()
-      response = requests.get(url, proxies={proxy[1]:proxy[0]})
+      time.sleep(5)
+      response = session.get(url, proxies=get_proxy(), headers={"User-Agent":"PostmanRuntime/7.42.0"})
       response.raise_for_status()
       return response.text
    except requests.RequestException as e:
       print(f"Error while fetching HTML content from the following URL {url} : {e}")
       return None 
+
+'''
+Functionality to generate a proxy for each of our requests 
+
+Args:
+   None 
+   
+Returns: 
+   proxies (dict) - dictionary containing proxies    
+''' 
+def get_proxy():
+   swiftshadow = Proxy(autoRotate=True)
+   proxy = swiftshadow.proxy() 
+   proxies = {proxy[1]:proxy[0]}
+   logging.info(f"Proxies generated: {proxies}")
+   
+      
+      
    
    
    
