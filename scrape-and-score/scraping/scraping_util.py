0639bbb940abcd62223462272c9c3699e76ee99e
Kellen Bavis <kellenbavis10@gmail.com>
2024-10-07 00:01:20 -0400

    feat: removed un-needed library utilization

--- scrape-and-score/scraping/scraping_util.py
+++ scrape-and-score/scraping/scraping_util.py
@@ -1,7 +1,7 @@
 import requests
 import logging 
-from swiftshadow.classes import Proxy
 import time 
+from proxy import proxy
 session = requests.Session() 
 
 '''
@@ -16,30 +16,16 @@ Returns:
 
 def fetch_page(url: str):
    try:
-      #TODO: Implement Rotating Proxy Logic (FFM-12)
       logging.info(f"Fetching raw HTML from the following URL: {url}")
+      
       time.sleep(3)
-      response = session.get(url, proxies=get_proxy(), headers={"User-Agent":"PostmanRuntime/7.42.0"})
+      response = session.get(url, proxies=proxy.get_proxy())
       response.raise_for_status()
       return response.text
    except requests.RequestException as e:
       print(f"Error while fetching HTML content from the following URL {url} : {e}")
       return None 
 
-'''
-Functionality to generate a proxy for each of our requests 
-
-Args:
-   None 
-   
-Returns: 
-   proxies (dict) - dictionary containing proxies    
-''' 
-def get_proxy():
-   swiftshadow = Proxy(autoRotate=True)
-   proxy = swiftshadow.proxy() 
-   proxies = {proxy[1]:proxy[0]}
-   logging.info(f"Proxies generated: {proxies}")
    
       
       
