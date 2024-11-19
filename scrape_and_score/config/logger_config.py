5e686b2b9bf8dd233f8e68d4526c7d31b3ef763f
Kellen Bavis <kellenbavis10@gmail.com>
2024-11-18 19:02:36 -0500

    chore: added comment

--- scrape_and_score/config/logger_config.py
+++ scrape_and_score/config/logger_config.py
@@ -1,5 +1,9 @@
 import logging;
 
+'''
+Functionality to configure our logging format 
+
+'''
 def configure_logging():
     logging.basicConfig(level=logging.INFO,  
                         format='%(asctime)s - %(levelname)s - %(message)s',
