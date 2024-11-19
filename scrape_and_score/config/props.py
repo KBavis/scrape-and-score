b8d0dce8f2b6caf4a2a241ca257a7338a6418880
Kellen Bavis <kellenbavis10@gmail.com>
2024-11-18 19:05:38 -0500

    chore: added comment

--- scrape_and_score/config/props.py
+++ scrape_and_score/config/props.py
@@ -1,5 +1,14 @@
 import yaml
 
+'''
+Functionality to load our configurations 
+
+Args: 
+   file_path(str): path containing configurations 
+
+Returns:
+   loaded configurations
+'''
 def load_configs(file_path="./resources/application.yaml"):
    with open(file_path, 'r') as file:
       return yaml.safe_load(file)
