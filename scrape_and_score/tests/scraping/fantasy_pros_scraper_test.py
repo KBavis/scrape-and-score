26bea68bb747fdf3b4242a8c3efea26152c0844f
Kellen Bavis <kellenbavis10@gmail.com>
2024-11-22 23:20:38 -0500

    test: added unit tests for fantasy pros scraper

--- scrape_and_score/tests/scraping/fantasy_pros_scraper_test.py
+++ scrape_and_score/tests/scraping/fantasy_pros_scraper_test.py
@@ -3,76 +3,113 @@ from bs4 import BeautifulSoup
 from scraping import fantasy_pros_scraper 
 
 
-soup = None
+@patch('scraping.fantasy_pros_scraper.construct_url', return_value = 'URL')
+@patch('scraping.fantasy_pros_scraper.fetch_page', return_value = None)
+@patch('scraping.fantasy_pros_scraper.get_depth_chart')
+@patch('scraping.fantasy_pros_scraper.BeautifulSoup')
+def test_scrape_returns_expected_data(mock_soup, mock_get_depth_chart, mock_fetch_page, mock_construct_url): 
+    base_url = 'https://www.fantasypros.com/nfl/depth-chart/{TEAM}.php'
+    teams = ['Indianapolis Colts']
+    expected_data = [{'player_name': 'Anthony Richardson', 'position': 'QB', 'team': 'Indianapolis Colts'}]
+    mock_get_depth_chart.return_value = expected_data
+    mock_soup.return_value = None
+    
+    data = fantasy_pros_scraper.scrape(base_url, teams)
+    
+    assert data == expected_data
 
-def setUpModule():
-    """Module-level setup for initializing soup with sample HTML."""
-    global soup
-    html_content = """
-    <div class="team-list">
-        <input class="team-name" value="New England Patriots" />
-        <div class="position-list">
-            <h4 class="position-head">ECR Quarterbacks</h4>
-            <a class="fp-player-link" fp-player-name="Mac Jones"></a>
-        </div>
-        <div class="position-list">
-            <h4 class="position-head">ECR Running Backs</h4>
-            <a class="fp-player-link" fp-player-name="Rhamondre Stevenson"></a>
-            <a class="fp-player-link" fp-player-name="Ezekiel Elliott"></a>
-        </div>
-    </div>
-    <div class="team-list">
-        <input class="team-name" value="Kansas City Chiefs" />
-        <div class="position-list">
-            <h4 class="position-head">ECR Quarterbacks</h4>
-            <a class="fp-player-link" fp-player-name="Patrick Mahomes"></a>
-        </div>
-        <div class="position-list">
-            <h4 class="position-head">ECR Tight Ends</h4>
-            <a class="fp-player-link" fp-player-name="Travis Kelce"></a>
-        </div>
-    </div>
+@patch('scraping.fantasy_pros_scraper.construct_url', return_value = 'URL')
+@patch('scraping.fantasy_pros_scraper.fetch_page', return_value = None)
+@patch('scraping.fantasy_pros_scraper.get_depth_chart')
+@patch('scraping.fantasy_pros_scraper.BeautifulSoup')
+def test_scrape_calls_expected_functions(mock_soup, mock_get_depth_chart, mock_fetch_page, mock_construct_url): 
+    base_url = 'https://www.fantasypros.com/nfl/depth-chart/{TEAM}.php'
+    teams = ['Indianapolis Colts']
+    expected_data = [{'player_name': 'Anthony Richardson', 'position': 'QB', 'team': 'Indianapolis Colts'}]
+    mock_get_depth_chart.return_value = expected_data
+    mock_soup.return_value = None
+    
+    fantasy_pros_scraper.scrape(base_url, teams)
+    
+    mock_soup.assert_called_once()
+    mock_get_depth_chart.assert_called_once() 
+    mock_construct_url.assert_called_once() 
+    mock_fetch_page.assert_called_once()
+    
+    
+    
+def test_construct_url_constructs_correct_url(): 
+    base_url = 'https://www.fantasypros.com/nfl/depth-chart/{TEAM}.php'
+    expected_url = 'https://www.fantasypros.com/nfl/depth-chart/arizona-cardinals.php'
+    team = 'Arizona Cardinals'
+    
+    actual_url = fantasy_pros_scraper.construct_url(team, base_url)
+    
+    assert actual_url == expected_url
+
+    
+def test_get_depth_chart_returns_expected_depth_chart():
+    html = """
+    <html>
+        <body>
+            <table>
+                <tr>
+                    <td>QB</td>
+                    <td>Anthony Richardson</td>
+                </tr>
+                <tr>
+                    <td>RB</td>
+                    <td>Jonathan Taylor</td>
+                </tr>
+            </table>
+        </body>
+    </html>
     """
-    # Parse HTML content with BeautifulSoup and assign to the global variable
-    soup = BeautifulSoup(html_content, "html.parser")
-
-
-@patch('scraping.util.fetch_page')
-@patch('scraping.fantasy_pros_scraper.fetch_team_data')
-def test_scrape(mock_fetch_team_data, mock_fetch_page):
-   # arrange 
-   url = "https://fantasypros.com"
-   mock_html = "<html><body></body></html>"
-   mock_fetch_page.return_value = mock_html
-   expected_values = [
-         {'team': 'Team A', 'position': 'QB', 'player_name': 'Player 1'},
-         {'team': 'Team A', 'position': 'QB', 'player_name': 'Player 2'}
-   ]
-   mock_fetch_team_data.return_value = expected_values
-   
-   # act 
-   result = fantasy_pros_scraper.scrape(url)
-   
-   # assert 
-   assert result == expected_values
-   
-   
-   
-
-def test_fetch_team_data():
+    soup = BeautifulSoup(html, 'html.parser')
+    team = "Indianapolis Colts"
     
-    # arrange 
-    expected_output = [
-        {'team': 'New England Patriots', 'position': 'QB', 'player_name': 'Mac Jones'},
-        {'team': 'New England Patriots', 'position': 'RB', 'player_name': 'Rhamondre Stevenson'},
-        {'team': 'New England Patriots', 'position': 'RB', 'player_name': 'Ezekiel Elliott'},
-        {'team': 'Kansas City Chiefs', 'position': 'QB', 'player_name': 'Patrick Mahomes'},
-        {'team': 'Kansas City Chiefs', 'position': 'TE', 'player_name': 'Travis Kelce'},
+    expected_data = [
+        {"player_name": "Anthony Richardson", "position": "QB", "team": team},
+        {"player_name": "Jonathan Taylor", "position": "RB", "team": team}
     ]
+    
+    result = fantasy_pros_scraper.get_depth_chart(soup, team)
+    assert result == expected_data
+    
+    
 
-    # act 
-    actual_output = fantasy_pros_scraper.fetch_team_data(soup)
+def test_get_depth_chart_skips_rows_without_name_and_position():
+    html = """
+    <html>
+        <body>
+            <table>
+                <tr>
+                    <td>QB</td>
+                    <td>Anthony Richardson</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>RB</td>
+                    <td>Jonathan Taylor</td>
+                </tr>
+                <tr>
+                    <td>QB</td>
+                </tr>
+            </table>
+        </body>
+    </html>
+    """
+    soup = BeautifulSoup(html, 'html.parser')
+    team = "Indianapolis Colts"
     
-    # assert 
-    assert actual_output == expected_output, f"Expected {expected_output}, got {actual_output}"
+    # expected data doesn't include empty cells
+    expected_data = [
+        {"player_name": "Anthony Richardson", "position": "QB", "team": team},
+        {"player_name": "Jonathan Taylor", "position": "RB", "team": team}
+    ]
     
+    result = fantasy_pros_scraper.get_depth_chart(soup, team)
+    assert result == expected_data
