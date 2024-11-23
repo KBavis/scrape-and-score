0db6238981807d00c11ed1aff89fb327bd72ee60
Kellen Bavis <kellenbavis10@gmail.com>
2024-11-23 00:23:14 -0500

    refactor: updated main.py to use new implementation of get_config

--- scrape_and_score/__main__.py
+++ scrape_and_score/__main__.py
@@ -1,7 +1,7 @@
 from config import configure_logging
 from scraping import scrape_fantasy_pros
 from scraping import pfr_scraper as pfr
-from config import load_configs
+from config import load_configs, get_config
 import logging
 
 
@@ -10,21 +10,19 @@ import logging
    persisting of data, generation of model, generation of predictions, and generating output
 '''
 def main():
-   #Set Up Global Logger Configurations
+   
    configure_logging()
+   load_configs() 
    
-   #Load Configurations from YAML file
-   config = load_configs() 
-   
-   template_url = config['website']['fantasy-pros']['urls']['depth-chart']
-   teams = [team['name'] for team in config['nfl']['teams']] # extract teams from configs
+   template_url = get_config('website.fantasy-pros.urls.depth-chart')
+   teams = [team['name'] for team in get_config('nfl.teams')] # extract teams from configs
    
    #Fetch relevant NFL teams & players
    teams_and_players = scrape_fantasy_pros(template_url, teams)
    logging.info(f"Successfully fetched {len(teams_and_players)} unique fantasy relevant players and their corresponding teams")
    
    #Fetch relevant team and player metrics 
-   team_metrics, player_metrics = pfr.scrape(teams_and_players)
+   team_metrics, player_metrics = pfr.scrape(teams_and_players, teams)
    logging.info(f"Successfully retrieved metrics for {len(team_metrics)} teams and {len(player_metrics)} players")
    
 # Entry point for the script
