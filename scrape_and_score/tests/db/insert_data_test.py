e9ad2cc964a111da7086f5767ff6a801e8346d9e
Kellen Bavis <kellenbavis10@gmail.com>
2024-11-29 23:00:08 -0500

    test: added unit test to finalize testing for insert_players in insert_data.py

--- scrape_and_score/tests/db/insert_data_test.py
+++ scrape_and_score/tests/db/insert_data_test.py
@@ -8,3 +8,13 @@ def test_insert_players_calls_insert_player_expected_number_of_times(mock_insert
    insert_data.insert_players(players)
    
    assert mock_insert_player.call_count == 2
+
+
+@patch('db.insert_data.insert_player', side_effect = Exception('Unable to insert the following player: Kyler Murray'))
+def test_insert_players_when_exceptions_occurs(mock_insert_player): 
+   players = ['Player One', 'Player Two']
+   
+   try:
+      insert_data.insert_players(players)
+   except Exception as e: 
+      assert str(e) == "Unable to insert the following player: Kyler Murray"
