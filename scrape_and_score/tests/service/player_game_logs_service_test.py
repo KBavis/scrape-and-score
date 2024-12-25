224443460730eaa70e57ddf7f448ea9c8897bb32
Kellen Bavis <kellenrbavis@gmail.com>
2024-12-25 14:59:47 -0500

    test: added unit tests to validate functionality regarding checking if a player game log persisted before inserting

--- scrape_and_score/tests/service/player_game_logs_service_test.py
+++ scrape_and_score/tests/service/player_game_logs_service_test.py
@@ -2,11 +2,174 @@ from service import player_game_logs_service
 from unittest.mock import patch 
 import pandas as pd
 
+@patch('service.player_game_logs_service.fetch_data.fetch_player_game_log_by_pk', return_value = {"player_id": 12, "week": 12, "year": 2024})
+def test_is_game_log_persisted_returns_true(mock_fetch_player_game_log_by_pk):
+  game_log_pk = {"player_id": 12, "week": 12, "year": 2024}
+  assert player_game_logs_service.is_game_log_persisted(game_log_pk) == True
+
+@patch('service.player_game_logs_service.fetch_data.fetch_player_game_log_by_pk', return_value = None)
+def test_is_game_log_persisted_returns_false(mock_fetch_player_game_log_by_pk): 
+  game_log_pk = {"player_id": 12, "week": 12, "year": 2024}
+  assert player_game_logs_service.is_game_log_persisted(game_log_pk) == False
+
+@patch('service.player_game_logs_service.fetch_data.fetch_player_game_log_by_pk', return_value = None)
+def test_is_game_log_persisted_calls_expected_functions(mock_fetch_player_game_log_by_pk): 
+   player_game_logs_service.is_game_log_persisted({"player_id": 12})
+   mock_fetch_player_game_log_by_pk.assert_called_once_with({'player_id': 12})
+
+@patch('service.player_game_logs_service.is_game_log_persisted', return_value = True)
+@patch('service.player_game_logs_service.service_util.extract_year_from_date', return_value=2024)
+@patch('service.player_game_logs_service.get_player_id_by_name', return_value = 14)
+def test_remove_previously_inserted_games_skips_removal_when_not_recent_games(mock_get_player_id, mock_extract_year, mock_is_game_log_persisted):
+   data = {
+      'week': [1, 2],
+      'date': ['2024-09-10', '2024-09-10'],
+      'game_location': ['@', '@'],
+      'opp': ['IND', 'IND'],
+      'result': ['W', 'W'],
+      'team_pts': [24, 24],
+      'opp_pts': [10, 10],
+      'cmp': [20, 20],
+      'att': [30, 30],
+      'pass_yds': [250, 250],
+      'pass_td': [2, 2],
+      'int': [1, 1],
+      'rating': [98.5, 100],
+      'sacked': [2, 2],
+      'rush_att': [5, 5],
+      'rush_yds': [30, 6],
+      'rush_td': [1, 2]
+   }
+   player_metrics = [{'player': 'My Player', 'player_metrics': pd.DataFrame(data=data)}]
+
+   player_game_logs_service.remove_previously_inserted_games(player_metrics, [])
+
+   assert mock_is_game_log_persisted.call_count == 0
+
+
+@patch('service.player_game_logs_service.is_game_log_persisted', return_value = True)
+@patch('service.player_game_logs_service.service_util.extract_year_from_date', return_value=2024)
+@patch('service.player_game_logs_service.get_player_id_by_name', return_value = 14)
+def test_remove_previously_inserted_games_calls_expected_functions(mock_get_player_id, mock_extract_year, mock_is_game_log_persisted):
+   data = {
+      'week': [1],
+      'date': ['2024-09-10'],
+      'game_location': ['@'],
+      'opp': ['IND'],
+      'result': ['W 24-10'],
+      'team_pts': [24],
+      'opp_pts': [10],
+      'cmp': [20],
+      'att': [30],
+      'pass_yds': [250],
+      'pass_td': [2],
+      'int': [1],
+      'rating': [98.5],
+      'sacked': [2],
+      'rush_att': [5],
+      'rush_yds': [30],
+      'rush_td': [1]
+   }
+   player_metrics = [{'player': 'My Player', 'player_metrics': pd.DataFrame(data=data)}]
+
+   player_game_logs_service.remove_previously_inserted_games(player_metrics, [])
+
+   mock_get_player_id.assert_called_once()
+   mock_extract_year.assert_called_once()
+   mock_is_game_log_persisted.assert_called_once_with({'player_id': 14, 'week': 1, 'year': 2024})
+
+
+
+@patch('service.player_game_logs_service.is_game_log_persisted', return_value = True) # game logs persisted, so should remove
+@patch('service.player_game_logs_service.service_util.extract_year_from_date', return_value=2024)
+@patch('service.player_game_logs_service.get_player_id_by_name', return_value = 14)
+def test_remove_previously_inserted_games_deletes_entries_from_player_metrics(mock_get_player_id, mock_extract_year, mock_is_game_log_persisted): 
+   data = {
+      'week': [1],
+      'date': ['2024-09-10'],
+      'game_location': ['@'],
+      'opp': ['IND'],
+      'result': ['W 24-10'],
+      'team_pts': [24],
+      'opp_pts': [10],
+      'cmp': [20],
+      'att': [30],
+      'pass_yds': [250],
+      'pass_td': [2],
+      'int': [1],
+      'rating': [98.5],
+      'sacked': [2],
+      'rush_att': [5],
+      'rush_yds': [30],
+      'rush_td': [1]
+   }
+   player_metrics = [{'player': 'My Player', 'player_metrics': pd.DataFrame(data=data)}]
+
+   player_game_logs_service.remove_previously_inserted_games(player_metrics, [])
+
+   assert len(player_metrics) == 0
+
+@patch('service.player_game_logs_service.is_game_log_persisted', return_value = False) # game logs NOT persisted, so shouldn't remove
+@patch('service.player_game_logs_service.service_util.extract_year_from_date', return_value=2024)
+@patch('service.player_game_logs_service.get_player_id_by_name', return_value = 14)
+def test_remove_previously_inserted_games_wont_delete_entries_from_player_metrics(mock_get_player_id, mock_extract_year, mock_is_game_log_persisted): 
+   data = {
+      'week': [1],
+      'date': ['2024-09-10'],
+      'game_location': ['@'],
+      'opp': ['IND'],
+      'result': ['W 24-10'],
+      'team_pts': [24],
+      'opp_pts': [10],
+      'cmp': [20],
+      'att': [30],
+      'pass_yds': [250],
+      'pass_td': [2],
+      'int': [1],
+      'rating': [98.5],
+      'sacked': [2],
+      'rush_att': [5],
+      'rush_yds': [30],
+      'rush_td': [1]
+   }
+   player_metrics = [{'player': 'My Player', 'player_metrics': pd.DataFrame(data=data)}]
+
+   player_game_logs_service.remove_previously_inserted_games(player_metrics, [])
+
+   assert len(player_metrics) == 1
+
+
+@patch('service.player_game_logs_service.insert_data.insert_wr_or_te_player_game_logs')
+@patch('service.player_game_logs_service.insert_data.insert_rb_player_game_logs')
+@patch('service.player_game_logs_service.insert_data.insert_qb_player_game_logs')
+@patch('service.player_game_logs_service.get_player_id_by_name')
+@patch('service.player_game_logs_service.remove_previously_inserted_games', return_value=None)
+def test_insert_multiple_players_game_logs_skips_insertion_when_previosuly_persisted(mock_remove, mock_get_player_id, mock_insert_qb, mock_insert_rb, mock_insert_wr):
+   player_game_logs_service.insert_multiple_players_game_logs([], [])
+   mock_remove.assert_called_once()
+   assert mock_get_player_id.call_count == 0
+   assert mock_insert_qb.call_count == 0
+   assert mock_insert_rb.call_count == 0
+   assert mock_insert_wr.call_count == 0
+
 
 @patch('service.player_game_logs_service.get_player_id_by_name')
 @patch('service.player_game_logs_service.get_qb_game_log_tuples')
 @patch('service.player_game_logs_service.insert_data.insert_qb_player_game_logs')
-def test_insert_multiple_player_game_logs_calls_expected_functions_when_position_is_qb(mock_insert_qb_game_logs, mock_get_qb_game_log_tuples, mock_get_player_id_by_name): 
+@patch('service.player_game_logs_service.remove_previously_inserted_games', return_value = None)
+def test_insert_multiple_player_game_logs_skips_insertion_when_game_logs_already_persisted(mock_remove, mock_insert_qb_game_logs, mock_get_qb_game_log_tuples, mock_get_player_id_by_name):
+   player_game_logs_service.insert_multiple_players_game_logs([], [])
+   assert mock_get_player_id_by_name.call_count == 0
+   assert mock_insert_qb_game_logs.call_count == 0
+   assert mock_get_qb_game_log_tuples.call_count == 0
+   mock_remove.assert_called_once()
+   
+
+@patch('service.player_game_logs_service.get_player_id_by_name')
+@patch('service.player_game_logs_service.get_qb_game_log_tuples')
+@patch('service.player_game_logs_service.insert_data.insert_qb_player_game_logs')
+@patch('service.player_game_logs_service.remove_previously_inserted_games', return_value = None)
+def test_insert_multiple_player_game_logs_calls_expected_functions_when_position_is_qb(mock_remove, mock_insert_qb_game_logs, mock_get_qb_game_log_tuples, mock_get_player_id_by_name): 
    mock_df = pd.DataFrame(data = [{'Hi'}])
    depth_charts = []
    player_metrics = [{'player': 'Anthony Richardson', 'position': 'QB', 'player_metrics': mock_df}]
@@ -20,13 +183,15 @@ def test_insert_multiple_player_game_logs_calls_expected_functions_when_position
    mock_insert_qb_game_logs.assert_called_once()
    mock_get_player_id_by_name.assert_called_once()
    mock_get_qb_game_log_tuples.assert_called_once() 
+   mock_remove.assert_called_once()
    
    
    
 @patch('service.player_game_logs_service.get_player_id_by_name')
 @patch('service.player_game_logs_service.get_rb_game_log_tuples')
 @patch('service.player_game_logs_service.insert_data.insert_rb_player_game_logs')
-def test_insert_multiple_player_game_logs_calls_expected_functions_when_position_is_rb(mock_insert_rb_game_logs, mock_get_rb_game_log_tuples, mock_get_player_id_by_name):
+@patch('service.player_game_logs_service.remove_previously_inserted_games', return_value = None)
+def test_insert_multiple_player_game_logs_calls_expected_functions_when_position_is_rb(mock_remove, mock_insert_rb_game_logs, mock_get_rb_game_log_tuples, mock_get_player_id_by_name):
    mock_df = pd.DataFrame(data = [{'Hi'}])
    depth_charts = []
    player_metrics = [{'player': 'Jonathon Taylor', 'position': 'RB', 'player_metrics': mock_df}]
@@ -45,7 +210,8 @@ def test_insert_multiple_player_game_logs_calls_expected_functions_when_position
 @patch('service.player_game_logs_service.get_player_id_by_name')
 @patch('service.player_game_logs_service.get_wr_or_te_game_log_tuples')
 @patch('service.player_game_logs_service.insert_data.insert_wr_or_te_player_game_logs')
-def test_insert_multiple_player_game_logs_calls_expected_functions_when_position_is_wr_or_te(mock_insert_wr_game_logs, mock_get_wr_game_log_tuples, mock_get_player_id_by_name): 
+@patch('service.player_game_logs_service.remove_previously_inserted_games', return_value = None)
+def test_insert_multiple_player_game_logs_calls_expected_functions_when_position_is_wr_or_te(mock_remove, mock_insert_wr_game_logs, mock_get_wr_game_log_tuples, mock_get_player_id_by_name): 
    mock_df = pd.DataFrame(data = [{'Hi'}])
    depth_charts = []
    player_metrics = [{'player': 'Josh Downs', 'position': 'WR', 'player_metrics': mock_df}]
@@ -62,7 +228,8 @@ def test_insert_multiple_player_game_logs_calls_expected_functions_when_position
  
    
 @patch('service.player_game_logs_service.get_player_id_by_name')
-def test_insert_multiple_player_game_logs_raises_exception_when_unknown_posiiton_found(mock_get_player_id):
+@patch('service.player_game_logs_service.remove_previously_inserted_games', return_value = None)
+def test_insert_multiple_player_game_logs_raises_exception_when_unknown_posiiton_found(mock_remove, mock_get_player_id):
    mock_df = pd.DataFrame(data = [{'Hi'}])
    depth_charts = []
    invalid_position = 'OL'
@@ -79,7 +246,8 @@ def test_insert_multiple_player_game_logs_raises_exception_when_unknown_posiiton
 @patch('service.player_game_logs_service.insert_data.insert_rb_player_game_logs')
 @patch('service.player_game_logs_service.insert_data.insert_qb_player_game_logs')
 @patch('service.player_game_logs_service.get_player_id_by_name')
-def test_insert_multiple_player_game_logs_skips_players_with_empty_metrics(mock_get_player_id, mock_insert_qb, mock_insert_rb, mock_insert_wr):
+@patch('service.player_game_logs_service.remove_previously_inserted_games', return_value = None)
+def test_insert_multiple_player_game_logs_skips_players_with_empty_metrics(mock_remove, mock_get_player_id, mock_insert_qb, mock_insert_rb, mock_insert_wr):
    mock_df = pd.DataFrame(data = [])
    depth_charts = []
    player_metrics = [{'player': 'Josh Downs', 'position': 'WR', 'player_metrics': mock_df}]
