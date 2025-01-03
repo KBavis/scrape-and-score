3b2a0be993f00d1ff604b8d17236bc6ad5f3e95e
Kellen Bavis <kellenbavis10@gmail.com>
2025-01-02 22:23:52 -0500

    feat: altered sql to account for rush/pass specific rankings in team table

--- scrape_and_score/sql/create_team_table.sql
+++ scrape_and_score/sql/create_team_table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE team (
     team_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    offense_rank INT,
-    defense_rank INT
+    off_rush_rank INT,
+    off_pass_rank INT,
+    def_rush_rank INT,
+    def_pass_rank INT
 );
