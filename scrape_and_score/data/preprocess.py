1cfe2bfd8de4ec7f36cbfc675b79d76de3397c97
Kellen Bavis <kellenbavis10@gmail.com>
2025-01-09 22:34:24 -0500

    feat; added encoding and ratio of rankings based on matchup

--- scrape_and_score/data/preprocess.py
+++ scrape_and_score/data/preprocess.py
@@ -23,12 +23,19 @@ def pre_process_data():
    filtered_df = filter_data(df)
    
    transformed_data = transform_data(filtered_df)
-   logging.info(transformed_data.describe(include='all'))
    
-   plot(transformed_data['log_fantasy_points'], 'log_fantasy_points_distribution')
-   plot(transformed_data['log_avg_fantasy_points'], 'log_avg_fantasy_points_distribution')
+   encoded_data = encode_positions(transformed_data)
    
-   create_plots(transformed_data)
+   data_with_ratio_ranks = get_position_relevant_ranks(encoded_data)
+   
+   # plot(transformed_data['log_fantasy_points'], 'log_fantasy_points_distribution')
+   # plot(transformed_data['log_avg_fantasy_points'], 'log_avg_fantasy_points_distribution')
+   
+   #TODO: Consider transforming ratio ranks so distributed better 
+   
+   #TODO: Create pre-processed_inputs variable that just has inputs 
+   
+   create_plots(data_with_ratio_ranks)
    
 
 '''
@@ -76,6 +83,44 @@ def transform_data(df: pd.DataFrame):
    logged_avg_fantasy_points = np.log1p(df['avg_fantasy_points'])
    df['log_avg_fantasy_points'] = logged_avg_fantasy_points
    return df
+
+
+'''
+Encode 'position' due to it being a categorical feature 
+
+Args:
+   df (pd.DataFrame): dataframe to transform 
+
+Returns:
+   None
+'''
+def encode_positions(df: pd.DataFrame): 
+   encoded_data = pd.get_dummies(df)
+   return encoded_data.reset_index(drop=True).astype(int) # reset indices
+
+
+'''
+Calculate ratio of relevant defensive ranking to relevant offensive ranking 
+
+Args:
+   df (pd.DataFrame): dataframe to generate relevant ranks for 
+
+Returns:
+   updated_df (pd.DataFrame): dataframe with correct rank ratios
+'''
+def get_position_relevant_ranks(df: pd.DataFrame): 
+   rush_ratio_rank = df.loc[df['position_RB'] == 1, 'def_rush_rank'] / df.loc[df['position_RB'] == 1, 'off_rush_rank']
+   df.loc[df['position_RB'] == 1, 'rush_ratio_rank'] = rush_ratio_rank
+   
+   
+   pass_positions = (df['position_WR'] == 1) | (df['position_TE'] == 1) | (df['position_QB'] == 1)
+   pass_ratio_rank = df.loc[pass_positions, 'def_pass_rank'] / df.loc[pass_positions, 'off_pass_rank']
+   df.loc[pass_positions, 'pass_ratio_rank'] = pass_ratio_rank
+   
+   df['rush_ratio_rank'] = df['rush_ratio_rank'].fillna(0)
+   df['pass_ratio_rank'] = df['pass_ratio_rank'].fillna(0)
+   return df
+
    
 
 '''
@@ -123,10 +168,10 @@ def create_plots(data: pd.DataFrame, output_file: str = 'featureplots.pdf'):
    f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, figsize =(15,3))
    ax1.scatter(data['avg_fantasy_points'],data['log_fantasy_points'])
    ax1.set_title('Log Fantasy Points and Avg Fantasy Points')
-   ax2.scatter(data['off_rush_rank'],data['log_fantasy_points'])
-   ax2.set_title('Log Fantasy Points and Off Rush Rank')
-   ax3.scatter(data['off_pass_rank'],data['log_fantasy_points'])
-   ax3.set_title('Log Fantasy Points and Off Pass Rank')
+   ax2.scatter(data['rush_ratio_rank'],data['log_fantasy_points'])
+   ax2.set_title('Log Fantasy Points and Rush Ratio Rank')
+   ax3.scatter(data['pass_ratio_rank'],data['log_fantasy_points'])
+   ax3.set_title('Log Fantasy Points and Pass Ratio Rank')
 
    plt.savefig(output_file)
 
