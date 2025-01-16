63de788b71f9a5e322a18017d31bed1666922412
Kellen Bavis <kellenbavis10@gmail.com>
2025-01-16 18:49:31 -0500

    feat: init prediction module for making a prediciton for a single player

---
+++ scrape_and_score/predictions/prediction.py
@@ -0,0 +1,13 @@
+import input
+
+'''
+Functionality to make a prediction for a single players # of fantasy points based on their matchup
+
+Args:
+   None 
+
+Returns:
+   None
+'''
+def make_single_player_prediction():
+   team_name, player_name = input.get_user_input()
