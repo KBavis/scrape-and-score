a3ad5d5a0055cbdf853364cb1d7f9021ec2338c3
Kellen Bavis <kellenbavis10@gmail.com>
2025-01-18 12:37:12 -0500

    feat: finalized logic to calculate prediction

--- scrape_and_score/predictions/prediction.py
+++ scrape_and_score/predictions/prediction.py
@@ -1,56 +1,92 @@
 from . import input
 import pandas as pd
 from db import fetch_data
+from data import preprocess
+import logging
+from sklearn.preprocessing import StandardScaler
+from models.lin_reg import LinReg
+import numpy as np
 
 '''
 Functionality to make a prediction for a single players # of fantasy points based on their matchup
 
 Args:
-   None 
+   linear_regression (LinReg): linear regression model
 
 Returns:
    None
 '''
-def make_single_player_prediction():
+def make_single_player_prediction(linear_regressions: LinReg):
    team_name, player_name = input.get_user_input()
    
    # fetch model corresponding to players position 
-   inputs = fetch_data.fetch_inputs_for_prediction(team_name, player_name)
+   logging.info(f'Fetching independent variables corresponding to player {player_name}...')
+   independent_vars = fetch_data.fetch_inputs_for_prediction(team_name, player_name)
    
-   # call predict 
+   logging.info(f'Calculating ratio rank for player {player_name}')
+   inputs_with_ratio_rank = calc_rankings_ratio(independent_vars)
    
-
-
-def get_inputs_for_prediction(): 
-   # fetch player db record 
+   logging.info(f'Applying logarithm transformation to inputs for player {player_name}')
+   transformed_inputs, _, _, _= preprocess.transform_data(inputs_with_ratio_rank)
    
-   # fetch player game log db records 
+   position = transformed_inputs['position'].iloc[0]
    
-   # fetch players corresponding team db record 
+   cols = ['log_avg_fantasy_points', 'log_ratio_rank']
+   inputs = transformed_inputs[cols]
    
-   # fetch opposing teams db record
-   return None
+   scaled_inputs = scale_inputs(inputs)
+   
+   predict(linear_regressions,scaled_inputs, position)
+   
+   
+'''
+Make prediciton based on players posiiton and scaled inputs
+''' 
+def predict(linear_regressions: LinReg, scaled_inputs: np.array, position: str):
+   # get corresponding LinearRegression model 
+   lin_reg_model = None
+   if position == 'RB':
+      lin_reg_model = linear_regressions.rb_regression
+   elif position == 'QB':
+      lin_reg_model = linear_regressions.qb_regression
+   elif position == 'TE':
+      lin_reg_model = linear_regressions.te_regression
+   else:
+      lin_reg_model = linear_regressions.qb_regression
+   
+   value = lin_reg_model.predict(scaled_inputs)
+   logging.info(f'Predicted # of fantasy points: {np.exp(value)}')
+   
+'''
+Functionality to scale inputs 
+   
+Args:
+   df (pd.DataFrmae): data frame to scale inputs for 
+   
+Returns
+   inputs (pd.DataFrame): inputs from data frame
+''' 
+def scale_inputs(df: pd.DataFrame): 
+   scaler = StandardScaler() 
+   scaler.fit(df)
+   scaled = scaler.transform(df)
+   return scaled
 
 '''
-Apply logarithm transformations to inputs 
+Calculate players ranking ratio based on offensive rank & opposing defense ranking
 
 Args:
-   df (pd.DataFrame): data frame with inputs to transform 
+   inputs (pd.DataFrame): data frame to calc rankings for 
 
 Returns:
-   transformed_df (pd.DataFrame): transformed data frame
+   inputs (pd.DataFrame): data frame with calculated ratio rank
 '''
-def transform_inputs():
-   return None
-
-'''
-Scale inputs in order to properly predict # of fantasy points 
-
-Args:
-   inputs (df): data frame to scale 
-
-Returns:
-   scaled_inputs (df): scaled inputs
-'''
-def scale_inputs(inputs:pd.DataFrame):
-   return None
+def calc_rankings_ratio(inputs: pd.DataFrame):
+   if inputs['position'].iloc[0] == 'RB':
+      rush_ratio_rank = inputs['def_rush_rank'] / inputs['off_rush_rank']
+      inputs['rush_ratio_rank'] = rush_ratio_rank
+   else :
+      pass_ratio_rank = inputs['def_pass_rank'] / inputs['off_pass_rank']
+      inputs['pass_ratio_rank'] = pass_ratio_rank
+   
+   return inputs
