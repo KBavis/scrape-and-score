7516d340aabb096bd5705d6f3bb71b3e864b4ff6
Kellen Bavis <kellenbavis10@gmail.com>
2025-01-21 20:43:29 -0500

    feat: completed impl for fetching & inserting teams historical odds for a particular season

--- scrape_and_score/scraping/rotowire_scraper.py
+++ scrape_and_score/scraping/rotowire_scraper.py
@@ -3,6 +3,7 @@ import requests
 import logging
 from config import props
 from service import team_service
+from db import insert_data
 
 '''
 Fetch all historical odds for the current year
@@ -13,7 +14,7 @@ Args:
 Returns:
    None
 '''
-def fetch_all(): 
+def scrape_all(): 
    # fetch configs
    url = props.get_config('website.rotowire.urls.historical-odds') 
    curr_year = props.get_config('nfl.current-year')
@@ -26,6 +27,10 @@ def fetch_all():
    curr_year_data = df[df['season'] == str(curr_year)] 
    team_betting_odds_records = get_team_betting_odds_records(curr_year_data) 
    
+   # insert into our db
+   logging.info('Inserting all teams historical odds into our database')
+   insert_data.insert_teams_historical_odds(team_betting_odds_records)
+   
    
 '''
 Generate team betting odds records to persist into our database
@@ -36,28 +41,28 @@ Args:
 def get_team_betting_odds_records(curr_year_data: pd.DataFrame):
    # create team id mapping 
    mapping = create_team_id_mapping(curr_year_data[curr_year_data['week'] == '1'])
-   
-   betting_odds_records = []
-   
-   for index, row in curr_year_data.iterrows(): 
-      home_team_id = mapping[row['home_team_stats_id']]
-      away_team_id = mapping[row['visit_team_stats_id']]
-      home_team_score = row['home_team_score']
-      away_team_score = row['visit_team_score']
-      week = row['week']
-      year = row['season']
-      game_over_under = row['game_over_under']
-      favorite_team_id = mapping[row['favorite']]
-      spread = row['spread']
-      total_points = row['total']
-      over_hit = row['over_hit']
-      under_hit = row['under_hit']
-      favorite_covered = row['favorite_covered']
-      underdog_covered = row['underdog_covered']
-   return None
+   betting_odds_records = [
+   {
+      'home_team_id': mapping[row['home_team_stats_id']],
+      'away_team_id': mapping[row['visit_team_stats_id']],
+      'home_team_score': row['home_team_score'],
+      'away_team_score': row['visit_team_score'],
+      'week': row['week'],
+      'year': row['season'],
+      'game_over_under': row['game_over_under'],
+      'favorite_team_id': mapping[row['favorite']],
+      'spread': row['spread'],
+      'total_points': row['total'],
+      'over_hit': row['over_hit'],
+      'under_hit': row['under_hit'],
+      'favorite_covered': row['favorite_covered'],
+      'underdog_covered': row['underdog_covered'],
+   }
+   for _, row in curr_year_data.iterrows()
+]
+   return betting_odds_records
    
 
-  
 '''
 Create a mapping of a teams acronym to corresponding team ID in our database
 
