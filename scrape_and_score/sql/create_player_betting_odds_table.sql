6d7986979cf4a1ce2b5c3da5d826b98eff362cf0
Kellen Bavis <kellenbavis10@gmail.com>
2025-02-01 15:45:56 -0500

    refactor: have player_id and player_name columns first

--- scrape_and_score/sql/create_player_betting_odds_table.sql
+++ scrape_and_score/sql/create_player_betting_odds_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE player_betting_odds (
+	player_id INT NOT NULL, 
+	player_name VARCHAR(100) NOT NULL,
 	label VARCHAR(100) NOT NULL,
 	cost INT NOT NULL, 
 	line FLOAT NOT NULL,
 	week INT NOT NULL, 
 	season INT NOT NULL,
-	player_id INT NOT NULL, 
-	player_name VARCHAR(100) NOT NULL,
 	PRIMARY KEY (week, season, label), 
 	FOREIGN KEY (player_id) REFERENCES player(player_id) ON DELETE CASCADE
 )
