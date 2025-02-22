ea1d80b41cf38ca4b24b4679b93236436c9e5ce2
Kellen Bavis <kellenbavis10@gmail.com>
2025-02-22 01:56:53 -0500

    feat: init neural network for predicting fantasy points
    NOTE: As of this commit, we are predicting within 3 points of the players fantasy points 50% of the time

---
+++ scrape_and_score/models/neural_net.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+class NeuralNetwork(nn.Module):
+   def __init__(self):
+      super().__init__()
+      
+      self.linear_relu_stack = nn.Sequential(
+         nn.Linear(45, 32), #TODO: Update this if the number of columns changes 
+         nn.ReLU(), 
+         nn.Linear(32, 1)
+      )
+      
+   
+   def forward(self, x: torch.Tensor):
+      """Execute the forward pass of our neural network 
+
+      Args:
+          x (torch.Tensor): tensor to pass through neural network 
+
+      Returns:
+          torch.Tensor : output of forward pass
+      """
+      logits = self.linear_relu_stack(x)
+      return logits
+   
