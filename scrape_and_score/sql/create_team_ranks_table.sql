2423611b0112522153a50910f83ce6e396a0cbb7
Kellen Bavis <kellenrbavis@gmail.com>
2025-02-27 23:52:24 -0500

    feat: SQL for creation of new table

---
+++ scrape_and_score/sql/create_team_ranks_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE team_ranks (
+	team_id INT NOT NULL, 
+	week INT NOT NULL, 
+	season INT NOT NULL, 
+	off_rush_rank INT,
+    off_pass_rank INT,
+    def_rush_rank INT,
+    def_pass_rank INT,
+	PRIMARY KEY (team_id, week, season),
+	FOREIGN KEY (team_id) REFERENCES team(team_id) ON DELETE CASCADE
+)
