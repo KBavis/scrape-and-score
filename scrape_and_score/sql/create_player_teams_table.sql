2f65ae2eea9f0fbb633bb5d6919c5736db071435
Kellen Bavis <kellenrbavis@gmail.com>
2025-03-03 23:33:17 -0500

    feat: schema for new player_teams table, used to map a player to a corresponding team for a set number of weeks in a season

--- scrape_and_score/sql/create_player_teams_table.sql
+++ scrape_and_score/sql/create_player_teams_table.sql
@@ -2,8 +2,9 @@ CREATE TABLE player_teams (
     player_id INT NOT NULL,
     team_id INT NOT NULL,
 	season INT NOT NULL, 
-	week INT NOT NULL,
-	PRIMARY KEY (player_id, team_id, season, week),
+	strt_wk INT NOT NULL, -- account for potential trades
+	end_wk INT NOT NULL, -- account for potential trades
+	PRIMARY KEY (player_id, team_id, season, strt_wk, end_wk),
     FOREIGN KEY (team_id) REFERENCES team(team_id) ON DELETE CASCADE,
 	FOREIGN KEY (player_id) REFERENCES player(player_id) ON DELETE CASCADE
 );
