a57fe3fa1ee9571857eddeb71729c43ff20ead3d
Kellen Bavis <kellenrbavis@gmail.com>
2025-03-12 00:02:56 -0400

    feat: added scaling and transformations required
    added cyclical nature for weeks and avoided scaling any categorical variables

--- scrape_and_score/data/nn_preprocess.py
+++ scrape_and_score/data/nn_preprocess.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from db import fetch_data as fetch
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import logging
 
 
 
@@ -10,10 +13,49 @@ def preprocess():
    parsed_df = parse_player_props(df)
 
    processed_df = pd.get_dummies(parsed_df, columns=['position'], dtype=int) #encode categoricla variable
-   #TODO: Drop the week column as well since this is NOT a independent variable
    processed_df.drop(columns=['player_id'], inplace=True) # drop un-needed values 
 
    return processed_df
+   
+
+def scale_and_transform(df: pd.DataFrame):
+   """
+   Functionality to scale and transform data frame and return respective inputs / ouputs 
+
+   Args:
+      df (pd.DataFrame): data frame containing X's & Y's 
+   
+   Returns:
+      np.array: numpy array containing scaled inputs 
+   """
+   logging.info("Attemptign to scale and transform DF in order to utilize in Neural Network training & testing")
+   scaler = StandardScaler()
+
+   print(df.columns)
+
+   # store independent variables in seperate data frame 
+   xs = df.drop(columns=['fantasy_points']).copy()
+
+
+   # independent variables to avoid scaling due to categorical nature
+   categorical_df = xs[['position_QB', 'position_RB', 'position_WR', 'position_TE']].copy()
+   categorical_vals = categorical_df.values
+
+   # independent variables to account for cyclical nature
+   cyclical_df = xs[['week', 'season']].copy()
+   cyclical_df['max_week'] = cyclical_df['season'].apply(lambda season: 17 if season < 2021 else 18)
+   cyclical_df['week_cos'] = np.cos(2 * np.pi * cyclical_df['week'] / cyclical_df['max_week'])
+   cyclical_df['week_sin'] = np.sin(2 * np.pi * cyclical_df['week'] / cyclical_df['max_week'])
+   cyclical_df = cyclical_df[['week_cos', 'week_sin']] # only keep transformed features
+   cyclical_week_vals = cyclical_df.values
+
+   # drop un-needed columns in original indep. variables 
+   xs = xs.drop(columns=['position_QB', 'position_RB', 'position_WR', 'position_TE', 'week', 'season'])
+
+   scaled_x_vals = scaler.fit_transform(xs.values)
+   
+   Xs = np.concatenate((scaled_x_vals, categorical_vals, cyclical_week_vals), axis=1)
+   return Xs
    
 
 
