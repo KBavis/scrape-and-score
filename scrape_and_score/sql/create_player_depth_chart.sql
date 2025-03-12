df1c810dd4be26de7f63cdf09740f399d094bd26
Kellen Bavis <kellenrbavis@gmail.com>
2025-03-11 23:00:29 -0400

    feat; SQL for creating player depth chart table

---
+++ scrape_and_score/sql/create_player_depth_chart.sql
@@ -0,0 +1,8 @@
+CREATE TABLE player_depth_chart (
+	player_id INT NOT NULL,
+	week INT NOT NULL,
+	season INT NOT NULL, 
+	depth_chart_pos INT,
+	PRIMARY KEY (week, season, player_id), 
+	FOREIGN KEY (player_id) REFERENCES player(player_id) ON DELETE CASCADE
+)
