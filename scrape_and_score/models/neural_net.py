45fc094b2382a50a84925c31826056fe8d4e755a
Kellen Bavis <kellenrbavis@gmail.com>
2025-03-14 19:41:35 -0400

    refactor: testing out new models

--- scrape_and_score/models/neural_net.py
+++ scrape_and_score/models/neural_net.py
@@ -2,15 +2,44 @@ import torch
 from torch import nn
 
 class NeuralNetwork(nn.Module):
-   def __init__(self):
+   def __init__(self, input_dim: int):
       super().__init__()
       
+      # self.linear_relu_stack = nn.Sequential(
+      #    nn.Linear(input_dim, 32), 
+      #    nn.ReLU(), 
+      #    nn.Linear(32, 1)
+      # )
+
+      # self.linear_relu_stack = nn.Sequential(
+      #    nn.Linear(45, 64),
+      #    nn.ReLU(), 
+      #    nn.Linear(64, 32),
+      #    nn.ReLU(),
+      #    nn.Linear(32, 16),
+      #    nn.ReLU(),
+      #    nn.Linear(16, 1)
+      # )
+      # self.linear_relu_stack = nn.Sequential(
+      #    nn.Linear(input_dim, 64), 
+      #    nn.ReLU(),
+      #    nn.Linear(64, 32),        
+      #    nn.ReLU(),
+      #    nn.Linear(32, 16),       
+      #    nn.ReLU(),
+      #    nn.Linear(16, 1)   
+      # )
       self.linear_relu_stack = nn.Sequential(
-         nn.Linear(46, 32), #TODO: Make input neurons configurable based on inputs
-         nn.ReLU(), 
-         nn.Linear(32, 1)
+         nn.Linear(input_dim, 128),  # Input layer
+         nn.ReLU(),
+         nn.BatchNorm1d(128),
+         nn.Dropout(0.3),
+         nn.Linear(128, 64),  # Hidden layer
+         nn.ReLU(),
+         nn.BatchNorm1d(64),
+         nn.Dropout(0.2),
+         nn.Linear(64, 1)     # Output layer
       )
-      
    
    def forward(self, x: torch.Tensor):
       """Execute the forward pass of our neural network 
