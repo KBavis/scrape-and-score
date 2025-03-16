c8b36c3055672428e445473447b2de286d34f9ba
Kellen Bavis <kellenrbavis@gmail.com>
2025-03-16 02:53:36 -0400

    feat: account for new normalized column

--- scrape_and_score/sql/create_player_table.sql
+++ scrape_and_score/sql/create_player_table.sql
@@ -2,5 +2,6 @@
 CREATE TABLE player (
     player_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    position VARCHAR(50) NOT NULL
+    position VARCHAR(50) NOT NULL,
+    normalized_name VARCHAR(100)
 );
