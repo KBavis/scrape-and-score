aff80aa32cec39056dab5a725a53560aec077151
Kellen Bavis <kellenrbavis@gmail.com>
2025-04-21 09:05:14 -0400

    refactor: add position arg

--- scrape_and_score/models/post_training.py
+++ scrape_and_score/models/post_training.py
@@ -6,15 +6,16 @@ import logging
 from datetime import datetime
 
 
-def feature_importance(model: torch.nn.Module, training_data_set: torch.utils.data.Dataset): 
+def feature_importance(model: torch.nn.Module, training_data_set: torch.utils.data.Dataset, position: str): 
     """
     Determine which features are most signficant in terms of the predictive value of i
 
     Args:
-        model (torch.nn.Module): _description_
-        training_data_set (torch.utils.data.Dataset): _description_
+        model (torch.nn.Module): neural network model
+        training_data_set (torch.utils.data.Dataset): the data set utilized for training
+        position (str): the specific position this model corresponds to 
     """
-    logging.info('Post Training: Determining feature signficance of our model')
+    logging.info(f'Post Training: Determining feature signficance of our {position} model')
 
 
     background = training_data_set.X[:100]
@@ -36,16 +37,23 @@ def feature_importance(model: torch.nn.Module, training_data_set: torch.utils.da
         'SHAP_Importance': np.abs(shap_values).mean(axis = 0)
     }).sort_values('SHAP_Importance', ascending=False)
 
-    print(f"Model Most Important Feautres: \n{feature_importance.head}")
+    print(f"{position} Model Most Important Feautres: \n{feature_importance.head}")
 
     # save feature significance table 
-    save_model_feature_significance_table(feature_importance)
+    save_model_feature_significance_table(feature_importance, position)
 
 
-def save_model_feature_significance_table(df: pd.DataFrame):
+def save_model_feature_significance_table(df: pd.DataFrame, position: str):
+    """
+    Helper function to save model feature significances 
+
+    Args:
+        df (pd.DataFrame): data frame containing training data 
+        position (str): position we are determing features signficance for
+    """
     # Generate a unique filename with a timestamp
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    file_name = f"feature_summary_table_{timestamp}.csv"
+    file_name = f"{position}_feature_summary_table_{timestamp}.csv"
 
     # Save DataFrame as CSV
     df.to_csv(file_name, index=False)
