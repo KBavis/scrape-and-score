ec00b264ef048cb8e052e5b5e90106d65581824b
Kellen Bavis <kellenrbavis@gmail.com>
2025-05-14 21:58:34 -0400

    feat: finalized impl of scraping upcoming NFL games

--- scrape_and_score/scraping/espn.py
+++ scrape_and_score/scraping/espn.py
@@ -2,8 +2,11 @@ from .util import fetch_page
 from config import props
 from bs4 import BeautifulSoup
 from datetime import datetime
-from db import fetch_data
+from db import fetch_data, insert_data
 import logging
+from . import pfr_scraper
+from constants import LOCATIONS, CITIES
+
 
 
 
@@ -62,22 +65,43 @@ def generate_and_persist(records: list, season: int, week: int):
         home_team_id = mapping[record['home_team']]
         away_team_id = mapping[record['away_team']]
 
+        home_team_name = fetch_data.fetch_team_name_by_id(home_team_id)
+        away_team_name = fetch_data.fetch_team_name_by_id(away_team_id)
+
+        day = record['game_date'].split(',')[0][0:3]
+
         parsed_date = datetime.strptime(record['game_date'], "%A, %B %d, %Y")
         formatted_date = parsed_date.strftime("%m/%d/%Y")
 
         home_team_rest_days = calculate_rest_days(home_team_id, season, week, parsed_date)
         away_team_rest_days = calculate_rest_days(away_team_id, season, week, parsed_date)
 
+        home_distance_traveled = 0
+        away_distance_traveled = round(pfr_scraper.calculate_distance(LOCATIONS[CITIES[home_team_name]], LOCATIONS[CITIES[away_team_name]]), 2)
+
         # generate two team game log records (one for each team) 
-        persistable_records.append({"team_id": home_team_id, "opp": away_team_id, "is_home": True, "game_date": formatted_date, "rest_days": home_team_rest_days})
-        persistable_records.append({"team_id": away_team_id, "opp": home_team_id, "is_home": False, "game_date": formatted_date, "rest_days": away_team_rest_days})
+        persistable_records.append({"team_id": home_team_id, "opp": away_team_id, "is_home": True, "game_date": formatted_date, "day": day, "rest_days": home_team_rest_days, "week": week, "year": season, "distance_traveled": home_distance_traveled})
+        persistable_records.append({"team_id": away_team_id, "opp": home_team_id, "is_home": False, "game_date": formatted_date, "day": day, "rest_days": away_team_rest_days, "week": week, "year": season, "distance_traveled": away_distance_traveled})
     
 
-    filter(persistable_records)
-    # TODO: Call insertion functionality after filtering (create new insert_upcoming_team_game_logs functionality in insert_data)
+    filtered_records = filter(persistable_records, season, week)
+    if filtered_records:
+        logging.info(f'Attempting to insert {len(filtered_records)} upcoming team_game_log records into our database for week {week} of the {season} NFL season.')
+        insert_data.insert_upcoming_team_game_logs(filtered_records) 
+    else:
+        logging.info('No new upcoming team_game_log records to be inserted; skipping insertion')
 
 
 def calculate_rest_days(team_id: int, season: int, week: int, curr_game_date: datetime):
+    """
+    Calculate the number of rest days for a particular team 
+
+    Args: 
+        team_id (int): the team ID to calc rest days for 
+        season (int): relevant season
+        week (int): relevant week 
+        curr_game_date (int): game date of current game 
+    """
 
     # extract date from last weeks game 
     prev_game_date = fetch_data.fetch_game_date_from_team_game_log(season, week - 1, team_id)
@@ -103,23 +127,40 @@ def filter(records: list, season: int, week: int):
         records (list): list of records to filter through 
         season (int): relevant season 
         week (int): relevant week 
+    
+    Returns:
+        list: filtered records (i.e not already persisted)
     """
 
-
+    filtered_records = []
+    
     for record in records:
         pk = {"team_id": record['team_id'], "week": week, "year": season}
+        team_game_log = fetch_data.fetch_team_game_log_by_pk(pk)
 
-        #TODO: This fetch_game-log_by_pk function currently is out dated. Update this function to specify individual attributes (ensuring we don't break any existing functionality in process) and ensure we include game date as well
-        game_log = fetch_data.fetch_team_game_log_by_pk(pk) 
-
-        if game_log:
-            # check if modification occured to game date 
-            # if changes occured, update team game log record in DB and filter out of records 
-            # if no changes occured, log the below message
-            logging.info("Team Game Log [team_id={},week={},season={}] already persisted & no updates required; skipping insertion")
-        else:
-            # no record exists, so we are free to insert
+        if team_game_log is None:
+            logging.info(f'Team Game Log [team_id={record['team_id']},week={week},season={season}] not persisted; appending to filtered records')
+            filtered_records.append(record)
             continue
+        
+        
+        # determine if game date has changed
+        persisted_game_date = team_game_log['game_date']
+        scraped_game_date = record['game_date']
+        scraped_datetime = datetime.strptime(scraped_game_date, '%m/%d/%Y').date()
+
+        # filter out record if already persisted and no updates required
+        if persisted_game_date == scraped_datetime:
+            logging.info(f"Team Game Log [team_id={record['team_id']},week={week},season={season}] already persisted & no updates required; filtering out record")
+            continue
+            
+        
+        # update game date in DB if changed since last insertion
+        logging.info(f"Team Game Log [team_id={record['team_id']},week={week},season={season}] has a game_date persisted of {persisted_game_date}, but the scraped date was {scraped_game_date}; updating team game log record with up-to-date game date.")
+        insert_data.update_team_game_log_game_date(scraped_game_date, pk) 
+    
+    return filtered_records
+
 
 
     
